f8cb5f87969f14a15e386f37b3e2d1b69764ec0f
shesh2509 <76209616+shesh2509@users.noreply.github.com>
2023-07-18 18:25:47 +0530

    Update requestMethods.js

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const BASE_URL = "http://localhost:5000/api/"
-const BASE_URL = "https://shoppy-qdl3.onrender.com/api/"
+const BASE_URL = "https://shoppy-api-mrbu.onrender.com/api/"
 //const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YTY2MThlYWUyNzc4NDc1NzhmNDU5NCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4ODg4OTM0OCwiZXhwIjoxNjg5MTQ4NTQ4fQ.3GRs4H6d6eXTK6iYugXYlKP_QPBKHFWOkV0vJhiGMgM"
 //const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken
 
